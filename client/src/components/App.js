98c6c06cbdc201472e0c3905b0f258443444981c
Jschles1 <jschles1@ramapo.edu>
2018-06-15 23:19:58 -0400

    Set up react router

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,24 @@
 import React, { Component } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+const Header = () => <div>Header</div>;
+const Dashboard = () => <div>Dashboard</div>;
+const ShowsNew = () => <div>ShowsNew</div>;
+const Landing = () => <div>Landing</div>;
 
 class App extends Component {
   render() {
     return (
-      <div>Test</div>
+      <div>
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/shows" component={Dashboard} />
+            <Route exact path="/shows/new" component={ShowsNew} />
+          </div>
+        </BrowserRouter>
+      </div>
     );
   }
 }
