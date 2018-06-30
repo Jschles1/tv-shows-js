36705570f98c19c682f7711c6592c3e39a50ee29
Jschles1 <jschles1@ramapo.edu>
2018-06-30 17:27:32 -0400

    Disabled service worker

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { applyMiddleware, createStore } from "redux";
 import thunk from "redux-thunk";
-import registerServiceWorker from "./registerServiceWorker";
+// import registerServiceWorker from "./registerServiceWorker";
 
 import "semantic-ui-css/semantic.css";
 
@@ -22,4 +22,4 @@ ReactDOM.render(
   </Provider>,
   document.getElementById("root")
 );
-registerServiceWorker();
+// registerServiceWorker();
