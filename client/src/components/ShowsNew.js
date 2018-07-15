c2a6e242033d32b8b1f0254c6c64478d3266ea4d
Jschles1 <jschles1@ramapo.edu>
2018-07-15 16:07:12 -0400

    Moved ShowsForm into ShowNew

--- client/src/components/ShowsNew.js
+++ client/src/components/ShowsNew.js
@@ -1,12 +1,23 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import * as actions from "../actions";
-import ShowsForm from "./ShowsForm";
+import { Segment, Form, Button } from "semantic-ui-react";
 import ShowsFormConfirm from "./ShowsFormConfirm";
 
 class ShowsNew extends Component {
   state = {
-    displayConfirm: false
+    displayConfirm: false,
+    name: "",
+    network: "",
+    weekday: "",
+    showtime: ""
+  };
+
+  handleOnChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
   };
 
   handleOnSubmit = event => {
@@ -20,10 +31,21 @@ class ShowsNew extends Component {
   };
 
   render() {
-    return this.state.displayConfirm ? (
+    let { name, network, weekday, showtime, displayConfirm } = this.state;
+    return displayConfirm ? (
       <ShowsFormConfirm confirmNewShow={this.confirmNewShow} />
     ) : (
-      <ShowsForm handleOnSubmit={this.handleOnSubmit} />
+      <Segment>
+        <Form onSubmit={this.handleOnSubmit}>
+          <Form.Group widths="equal">
+            <Form.Input value={name} onChange={this.handleOnChange} name="name" label="Show Name" placeholder="Show Name" />
+            <Form.Input value={network} onChange={this.handleOnChange} name="network" label="Network" placeholder="Network" />
+            <Form.Input value={weekday} onChange={this.handleOnChange} name="weekday" label="Weekday" placeholder="Weekday" />
+            <Form.Input value={showtime} onChange={this.handleOnChange} name="showtime" label="Showtime" placeholder="Showtime" />
+          </Form.Group>
+          <Button type="submit">Add Show</Button>
+        </Form>
+      </Segment>
     );
   }
 }
